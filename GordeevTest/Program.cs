4a9625c3ec1dbf87bdb7aac907b1928f9588a94e
Vladislav301 <gordeevvlad151@gmail.com>
2024-11-29 19:13:21 +0900

    Add files via upload

---
+++ GordeevTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using InspectorLib;
+using System;
+
+public class Example
+{
+    public static void Main(string[] args)
+    {
+        FunctionInsp inspection = new FunctionInsp("Городская автоинспекция", "Иванов Иван Иванович");
+
+        Console.WriteLine($"Главный инспектор: {inspection.GetInspector()}");
+        Console.WriteLine($"Название автоинспекции: {inspection.GetCarInspection()}");
+        Console.WriteLine($"Сгенерированный госномер: {inspection.GenerateNumber('A')}");
+
+        inspection.AddWorker("Петров Петр Петрович");
+        Console.WriteLine($"Список сотрудников: {string.Join(", ", inspection.GetWorkers())}");
+
+        bool inspectorChanged = inspection.SetInspector("Петров Петр Петрович");
+        Console.WriteLine($"Инспектор изменен: {inspectorChanged}");
+        Console.WriteLine($"Новый главный инспектор: {inspection.GetInspector()}");
+
+
+        bool failedChange = inspection.SetInspector("Сидоров Сидор Сидорович"); 
+        Console.WriteLine($"Инспектор изменен: {failedChange}");
+
+    }
+}
+
+
